317236327760be0ec04fec9b3d8e7c5fcfa73a2e
Efe <h.efe_sonmez@hotmail.com>
2023-06-10 05:44:33 +0300

    feat: nav implement

--- cvapp/urls.py
+++ cvapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
-from cvapp.views import index
+from cvapp.views import index, contact
 
 urlpatterns = [
     path("", index, name='index'),
+    path("contact/", contact, name='contact'),
 ]
