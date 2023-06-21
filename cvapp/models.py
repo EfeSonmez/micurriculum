fe1a3c22563e1b866dce6ad9c5be59c7c54342aa
Efe <h.efe_sonmez@hotmail.com>
2023-06-21 05:55:53 +0300

    refactor: added dynamic site renderin

--- cvapp/models.py
+++ cvapp/models.py
@@ -2,27 +2,41 @@ from django.db import models
 
 # Create your models here.
 
-class GeneralSettings(models.Model):
+class GeneralSetting(models.Model):
     name = models.CharField(
         default='',
         max_length=254,
-        blank=True
+        blank=True,
+        verbose_name="Name",
     )
     description = models.CharField(
         default='',
         max_length=254,
-        blank=True
+        blank=True,
+        verbose_name="Description",
+        help_text='This is variable of the setting.'
     )
     parameter = models.CharField(
         default='',
         max_length=254,
-        blank=True
+        blank=True,
+        verbose_name="Parameter",
     )
     updated_date = models.DateTimeField(
         blank=True,
         auto_now=True,
+        verbose_name=" Updated Date",
     )
     created_date = models.DateTimeField(
         blank=True,
         auto_now_add=True,
+        verbose_name="Created Date",
     )
+
+    def __str__(self):
+        return f"general Setting: {self.name}"
+
+    class Meta:
+        verbose_name = "General Setting"
+        verbose_name_plural = "General Setting"
+        ordering = ('name',)
